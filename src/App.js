6178484e71b8925bbb4a2cc347cfeec0258d9729
1023002480 <xzen0011@studnet.monash.edu>
2021-06-30 20:36:10 +1000

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect } from 'react';
+import './App.css';
+import react from 'react';
+
+const App=()=>{
+  const color = [];
+  function addColor(){
+    for(var i = 1; i <33; i++){
+      for(var j = 1; j <33; j++){
+        for(var k = 1; k <33; k++){
+          color.push({red:8*i,green:8*j,blue:8*k});
+        }
+      }
+    }
+  }
+  addColor();
+  useEffect(()=>{
+    const mainArea = document.querySelector(".App-header");
+    for (var i = 1; i < 129; i++) {
+      for (var j = 0; j < 256; j++) {
+          var span = document.createElement('span');
+          var index = i*j;
+          span.style.backgroundColor='rgb('+ color[index].red+','+ color[index].green+','+color[index].blue+')';
+          mainArea.append(span);
+          span.style.top = i * 5 + "px";
+          span.style.left = j * 5 + "px";
+      }
+  }
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+      </div>
     </div>
   );
 }
