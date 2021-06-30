c7105c50a89b04da618693c7b28a3621ee7dc137
1023002480 <xzen0011@studnet.monash.edu>
2021-06-30 20:48:45 +1000

    code prettier

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
+import { useEffect } from "react";
+import "./App.css";
 
-import { useEffect } from 'react';
-import './App.css';
-import react from 'react';
-
-const App=()=>{
-  const color = [];
-  function addColor(){
-    for(var i = 1; i <33; i++){
-      for(var j = 1; j <33; j++){
-        for(var k = 1; k <33; k++){
-          color.push({red:8*i,green:8*j,blue:8*k});
+const App = () => {
+  useEffect(() => {
+    // Get All Possible Color CombinationS
+    const color = [];
+    function addColor() {
+      for (var i = 1; i < 33; i++) {
+        for (var j = 1; j < 33; j++) {
+          for (var k = 1; k < 33; k++) {
+            color.push({ red: 8 * i, green: 8 * j, blue: 8 * k });
+          }
         }
       }
     }
-  }
-  addColor();
-  useEffect(()=>{
+    addColor();
+    //Get Element className = "App-header"
     const mainArea = document.querySelector(".App-header");
     for (var i = 1; i < 129; i++) {
       for (var j = 0; j < 256; j++) {
-          var span = document.createElement('span');
-          var index = i*j;
-          span.style.backgroundColor='rgb('+ color[index].red+','+ color[index].green+','+color[index].blue+')';
-          mainArea.append(span);
-          span.style.top = i * 5 + "px";
-          span.style.left = j * 5 + "px";
+        //Create little Color Square Boxes
+        var span = document.createElement("span");
+        var index = i * j;
+        // Add a unique background color in the box
+        span.style.backgroundColor =
+          "rgb(" +
+          color[index].red +
+          "," +
+          color[index].green +
+          "," +
+          color[index].blue +
+          ")";
+        // Append this span tag
+        mainArea.append(span);
+        // Move it to the right position
+        span.style.top = i * 5 + "px";
+        span.style.left = j * 5 + "px";
       }
-  }
-  },[])
+    }
+  }, []);
   return (
     <div className="App">
-      <div className="App-header">
-      </div>
+      <div className="App-header"></div>
     </div>
   );
-}
+};
 
 export default App;
